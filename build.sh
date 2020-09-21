115404c7be2728445f103ffeca8223c163a62de4
Raphael Lehmann <raphael@rleh.de>
2020-09-21 23:37:30 +0000

    Setup CircleCI and Github Actions

--- build.sh
+++ build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-INSTALL="/opt/riscv-gcc"
-
 # Get sources
-#git clone --recursive --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain
 git clone --recursive https://github.com/riscv/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
 
 # Configure
-./configure --prefix="$INSTALL" --enable-multilib
+./configure --prefix=/opt/riscv-gcc
 
 # Build
-make linux -j8
+make -j4
